b512b3a80f1db0e8c999ff2ab7ea2b042ccfd6e9
Tobi <71647024+Tobi-D7@users.noreply.github.com>
2020-12-30 00:29:14 +0100

    Update main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,29 @@
+#include <3ds.h>
+#include <iostream>
+
+#include "gui.hpp"
+
+int main ()
+{
+    Gui::Init();
+    gfxInitDefault();
+    romfsInit();
+    cfguInit();
+
+    while (aptMainLoop())
+    {
+        hidScanInput();
+        u32 hDown hidKeysDown();
+        u23 hHeld hidKeysHeld();
+
+        if (hDown & KEY_START)
+        {
+            break;
+        }
+    }
+    Gui::exit();
+    gfxExit();
+    romfsExit();
+    cfguExit();
+
+}
