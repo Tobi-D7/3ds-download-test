e252ed17e8f82667bae02861c3950167a77ca085
Tobi <71647024+Tobi-D7@users.noreply.github.com>
2021-01-31 22:02:43 +0100

    --

---
+++ D7-Core/README.md
@@ -0,0 +1,11 @@
+### D7-Core (3ds)
+Basic stuff of our Projects.
+# Todo:
+
+( )Screenshot-Engine
+
+( )Basic UI stuff
+
+( )Massege-Engine(with animation)
+
+( )Logging
---
+++ D7-Core/ui.hpp
@@ -0,0 +1,17 @@
+#include "includes.hpp"
+#include "menu.hpp"
+
+namespace UI {
+    Result init(void);
+    void fini(void);
+    Result reinit(void);
+    void clearTextbuf(void);
+    namespace T3X {
+        Result loadSpriteSheet(const char * sheetpath, C2D_SpriteSheet &spritesheet);
+        Result unloadSpriteSheet(C2D_SpriteSheet &spritesheet);
+        void DrawSprite(C2D_SpriteSheet spritesheet, size_t imgindex, int x, int y, float Xscale = 1, float Yscale = 1);
+    }
+    Result loadFont(C2D_Font &bcfnt, const char *path = "");
+    Result unloadFont(C2D_Font &bcfnt);
+    void DrawonScreen(C3D_RenderTarget * screen);
+}
