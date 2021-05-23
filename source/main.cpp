0831ddb196244620999d8a131eb484048f1a1419
Tobi <tobid7@outlook.de>
2021-05-23 23:16:44 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,23 @@
 #include <3ds.h>
 #include <download.hpp>
 
+extern curl_off_t downloadTotal;
+extern curl_off_t downloadNow;
+
+bool dl = true;
 int main()
 {
     gfxInitDefault();
     consoleInit(GFX_BOTTOM, NULL);
-    downloadToFile("https://github.com/Creckeryop/3dsCookieClicker/archive/refs/heads/master.zip", "sdmc:/CC.zip");
+    char str[256];
     while (aptMainLoop())
     {
-    
+         if (dl) 
+         {
+                 downloadToFile("https://github.com/Creckeryop/3dsCookieClicker/archive/refs/heads/master.zip", "sdmc:/CC.zip");
+         } 
+         snprintf(str, sizeof(str), (%.2f%%), ((float)downloadNow/(float)downloadTotal) * 100.0f); 
+         pritf(str);
     }
     
 }
