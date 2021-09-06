57e6e61ccaa13dad9e41af4803bb983ad3d06568
Tobi <tobid7@outlook.de>
2021-09-06 13:26:24 +0200

    update newmain.cpp

--- source/newmain.cpp
+++ source/newmain.cpp
@@ -72,7 +72,7 @@ int main()
        // renderer.drawTop(spr1);
         //renderer.drawBottom(skyb, d7gfx::RenderContext::Mode::Spatial);
         renderer.drawBottom(cube, d7gfx::RenderContext::Mode::Spatial);
-        renderer.Render(true);
+        renderer.Render(false);
         renderer.setClearColor(sky);
     }
     d7gfx::ExitApp();
