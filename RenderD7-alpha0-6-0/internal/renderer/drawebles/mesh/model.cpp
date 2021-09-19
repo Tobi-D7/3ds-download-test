e415688c64dd4f84dd5398448697883460f79535
Tobi <tobid7@outlook.de>
2021-09-19 20:29:46 +0200

    Update model.cpp

--- RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/model.cpp
+++ RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/model.cpp
@@ -9,11 +9,13 @@ namespace d7gfx {
                 printf("Failed!\n");
                 return false;
             }
-            for (unsigned int m = 0; m < loader.LoadedMaterials.size(); m++)
-            {
-                 Material tm(loader.LoadedMaterials[m].Ka.X, loader.LoadedMaterials[m].Ka.Y, loader.LoadedMaterials[m].Ka.Z, loader.LoadedMaterials[m].Kd.X, loader.LoadedMaterials[m].Kd.Y, loader.LoadedMaterials[m].Ks.Z, loader.LoadedMaterials[m].Ks.X, loader.LoadedMaterials[m].Ks.Y, loader.LoadedMaterials[m].Ks.Z);
+            //for (unsigned int m = 0; m < loader.LoadedMaterials.size(); m++)
+            //{
+               //  Material tm(loader.LoadedMaterials[m].Ka.X, loader.LoadedMaterials[m].Ka.Y, loader.LoadedMaterials[m].Ka.Z, loader.LoadedMaterials[m].Kd.X, loader.LoadedMaterials[m].Kd.Y, loader.LoadedMaterials[m].Ks.Z, loader.LoadedMaterials[m].Ks.X, loader.LoadedMaterials[m].Ks.Y, loader.LoadedMaterials[m].Ks.Z);
+              //   this->setMaterial(tm);
+                 Material tm(loader.LoadedMaterials[0].Ka.X, loader.LoadedMaterials[0].Ka.Y, loader.LoadedMaterials[0].Ka.Z, loader.LoadedMaterials[0].Kd.X, loader.LoadedMaterials[0].Kd.Y, loader.LoadedMaterials[0].Ks.Z, loader.LoadedMaterials[0].Ks.X, loader.LoadedMaterials[0].Ks.Y, loader.LoadedMaterials[0].Ks.Z);
                  this->setMaterial(tm);
-            }
+          //  }
             for (unsigned int i = 0; i < loader.LoadedMeshes.size(); i++) {
                 printf("\x1b[3;1HLoading Model: %i/%i\x1b[K", i, (int)loader.LoadedMeshes.size());
                 objl::Mesh curMesh = loader.LoadedMeshes[i];
