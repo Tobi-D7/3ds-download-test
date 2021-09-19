33373aa45e96e75817b4997da8477bfb71b011a9
Tobi <tobid7@outlook.de>
2021-09-19 17:53:34 +0200

    Update material.hpp

--- RenderD7-alpha0-6-0/internal/renderer/drawebles/material.hpp
+++ RenderD7-alpha0-6-0/internal/renderer/drawebles/material.hpp
@@ -8,7 +8,7 @@ namespace d7gfx {
     public:
         
         Material();
-        Material(int kar, int kag, int kab, int kdr, int kdg, int kdb, int kdr, int kar, int ksb);
+        Material(int kar, int kag, int kab, int kdr, int kdg, int kdb, int ksr, int ksg, int ksb);
         Material(C3D_Material t_material);
         Material(const d7gfx::Material& t_source);
         virtual ~Material();
