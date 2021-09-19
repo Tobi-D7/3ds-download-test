485dd8b54a33eddf4d82367cdfd00d56043c8fcf
Tobi <tobid7@outlook.de>
2021-09-19 20:39:25 +0200

    Update model.cpp

--- RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/model.cpp
+++ RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/model.cpp
@@ -13,8 +13,10 @@ namespace d7gfx {
             //{
                //  Material tm(loader.LoadedMaterials[m].Ka.X, loader.LoadedMaterials[m].Ka.Y, loader.LoadedMaterials[m].Ka.Z, loader.LoadedMaterials[m].Kd.X, loader.LoadedMaterials[m].Kd.Y, loader.LoadedMaterials[m].Ks.Z, loader.LoadedMaterials[m].Ks.X, loader.LoadedMaterials[m].Ks.Y, loader.LoadedMaterials[m].Ks.Z);
               //   this->setMaterial(tm);
+              if (loader.LoadedMaterials.size() != 0)
                  Material tm(loader.LoadedMaterials[0].Ka.X, loader.LoadedMaterials[0].Ka.Y, loader.LoadedMaterials[0].Ka.Z, loader.LoadedMaterials[0].Kd.X, loader.LoadedMaterials[0].Kd.Y, loader.LoadedMaterials[0].Ks.Z, loader.LoadedMaterials[0].Ks.X, loader.LoadedMaterials[0].Ks.Y, loader.LoadedMaterials[0].Ks.Z);
                  this->setMaterial(tm);
+              }
           //  }
             for (unsigned int i = 0; i < loader.LoadedMeshes.size(); i++) {
                 printf("\x1b[3;1HLoading Model: %i/%i\x1b[K", i, (int)loader.LoadedMeshes.size());
