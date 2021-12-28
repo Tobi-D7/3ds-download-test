8eb83075e3c0092f8bd554f56a1ac1b4b527246a
Tobi <tobid7@outlook.de>
2021-12-28 12:50:51 +0100

    Update newmain.cpp

--- source/newmain.cpp
+++ source/newmain.cpp
@@ -38,11 +38,11 @@ int main()
     while (d7gfx::Mainloop())
     {
         hidScanInput();
-        printf("\x1b[5;1HFramerate: %s\x1b[K", RenderD7::GetFramerate().c_str());
+      /*  printf("\x1b[5;1HFramerate: %s\x1b[K", RenderD7::GetFramerate().c_str());
         printf("\x1b[6;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
 	printf("\x1b[7;1HGPU:     %6.2f%%\x1b[K", C3D_GetDrawingTime()*6.0f);
 	printf("\x1b[8;1HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
-
+*/
         u32 d7_hDown = hidKeysDown();
         u32 d7_hHeld = hidKeysHeld();
        /* if(d7_hHeld & KEY_CSTICK_DOWN) 
