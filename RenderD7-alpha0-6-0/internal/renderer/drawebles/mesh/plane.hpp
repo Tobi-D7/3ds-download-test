de7b899738fc06e0b76ff413ce39c57fca52600f
Tobi <tobid7@outlook.de>
2022-01-01 18:22:55 +0100

    Update plane.hpp

--- RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/plane.hpp
+++ RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/plane.hpp
@@ -1,1 +1,19 @@
 
+#pragma once
+#include "mesh.hpp"
+
+namespace d7gfx {
+    
+    class Plane: public d7gfx::Mesh {
+    public:
+        Plane(float t_length, float t_width);
+        void setLength(float t_length);
+        float getLength();
+        void setWidth(float t_width);
+        float getWidth();
+
+    private:
+        void update();
+        float m_length, m_width;
+    };
+}
