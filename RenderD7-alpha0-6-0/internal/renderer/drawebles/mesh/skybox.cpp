c84a84920e8347443ec878d014f3e04632612326
Tobi <tobid7@outlook.de>
2022-01-04 16:49:16 +0100

    Update skybox.cpp

--- RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/skybox.cpp
+++ RenderD7-alpha0-6-0/internal/renderer/drawebles/mesh/skybox.cpp
@@ -127,8 +127,10 @@ namespace d7gfx {
         C3D_TexBind(0, &sktex);
 	    C3D_TexEnv* env = C3D_GetTexEnv(0);
 	    C3D_TexEnvInit(env);
+            extern "C"
+            {
 	    C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, 0, 0);
 	    C3D_TexEnvFunc(env, C3D_Both, GPU_REPLACE);
-        
+            }
     }
 } /* d7gfx */
